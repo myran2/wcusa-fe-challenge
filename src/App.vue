7266da6ef4f77caf5ae79fe5a1a83351b33fc57b
Henry Gordon <henrytgordon@gmail.com>
2022-11-19 12:24:01 -0500

    commit local changes

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,36 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
   <router-view/>
 </template>
 
 <style lang="scss">
+body {
+  background-color: rgb(227, 240, 244);
+  background-attachment: fixed;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
-nav {
-  padding: 30px;
+  max-width: 786px;
+  margin: 0 auto;
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+  .home {
+    h1 {
+      text-align: center;
+      margin: 0;
+      margin-top: 3%;
     }
   }
 }
+
+.btn {
+  width: fit-content;
+  text-decoration: none;
+  border-radius: 25px;
+  display: inline-block;
+  border: 3px solid;
+  padding: 8px 40px;
+}
+
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,27 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <h1>Your most important Risks</h1>
+    <CategoryList :selectedIndex=1 />
+    <CardCategory v-for="category in CardCategoriesJson" :key="category.id" :categoryId=category.id :categoryName=category.name />
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import HelloWorld from '@/components/HelloWorld.vue' // @ is an alias to /src
+import CategoryList from '@/components/CategoryList.vue'
+import CardCategory from '@/components/CardCategory.vue'
+import CardCategoriesJson from '@/assets/categories.json'
 
 export default defineComponent({
   name: 'HomeView',
   components: {
-    HelloWorld
+    CategoryList,
+    CardCategory
+  },
+  setup () {
+    return {
+      CardCategoriesJson
+    }
   }
 })
 </script>
